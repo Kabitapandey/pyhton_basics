87e7c148addf1e755409ee2baf20d50ce6c405d5
Kabita <pandeykabita487@gmail.com>
2021-05-21 18:28:42 +0545

    Python basics

---
+++ pyhton_basics/dice_roll/dice.py
@@ -0,0 +1,9 @@
+from random import randint as r
+
+
+def dice_roll():
+    a = r(1, 6)
+    print('|', a, '|')
+
+
+dice_roll()
---
+++ pyhton_basics/dice_roll/myModule.py
@@ -0,0 +1,5 @@
+def greeting(name):
+    return 'hello'+name
+
+
+age = 21
